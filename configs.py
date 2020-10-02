8fac014ae83ab23f41b5b12a5b99ea812afd1faf
1160301020 <2518908628@QQ.COM>
2020-10-02 13:34:51 +1000

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,4 @@
 save_dir                    = '.'
 data_dir = {}
-data_dir['cifar']             = './filelists/cifar/' 
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
